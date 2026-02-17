5da20641e8cc8bbe4500e37cb81c86c520231da8
ConzZah <69615452+ConzZah@users.noreply.github.com>
2026-02-17 10:52:21 +0100

    v1.3
    - replaced echo with printf for portability
    - fixed a bug where the script would continue even tho the url was invalid

--- iadl.sh
+++ iadl.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
   #==============================================
-  # Project: iadl.sh v1.2
+  # Project: iadl.sh v1.3
   # Author:  ConzZah / (c) 2026
-  # Last Modification: 2/16/26 10:33 AM
+  # Last Modification: 2/17/26 10:47 AM
   #==============================================
 
 init () {
@@ -10,27 +10,27 @@ init () {
 deps="mktemp paste curl grep fzf sed cat cut tr"
 for dep in $deps; do
 ! command -v "$dep" >/dev/null && \
-{ echo; echo "--> MISSING DEPENDENCY: $dep"; echo; exit 1 ;}
+printf "%s" "\n--> MISSING DEPENDENCY: $dep\n\n" && exit 1
 done
 
 ## if $1 is -fn, set flag and shift. useful in scripts
 fn=""; [ "$1" = '-fn' ] && fn="y" && shift
 
 url=""; base_url="https://archive.org"
-[ -z "$1" ] && { echo; echo "--> PLS SUPPLY SOME ARCHIVE.ORG LINK"; echo; exit 1 ;}
+[ -z "$1" ] && printf "\n--> PLS SUPPLY SOME ARCHIVE.ORG LINK\n\n" && exit 1
 
 ## get url
 [ -n "$1" ] && url="$1"
 
 ## make sure url is valid
-echo "$url"| { ! grep -q "$base_url.*" && { echo ''; echo "--> THIS ISN'T A VALID ARCHIVE.ORG LINK"; echo; exit 1 ;} ;}
+printf "%s" "$url"| grep -vq "$base_url.*" && printf "\n --> THIS ISN'T A VALID ARCHIVE.ORG LINK\n\n" && exit 1
 
 ## should url contain 'details', replace it with 'download'
-echo "$url"| grep -q "$base_url/details.*" && url="$(echo "$url"| sed 's#details#download#')"
+printf "%s" "$url"| grep -q "$base_url/details.*" && url="$(printf "%s" "$url"| sed 's#details#download#')"
 
 ## create tmpdir if nonexistant.
 [ ! -f '.tmpdir' ] || [ ! -d "$(cat '.tmpdir')" ] && \
-tmpdir="$(mktemp -d)" && echo "$tmpdir" > '.tmpdir'
+tmpdir="$(mktemp -d)" && printf "%s" "$tmpdir" > '.tmpdir'
 
 [ -f ".tmpdir" ] && [ -d "$(cat '.tmpdir')" ] && tmpdir="$(cat '.tmpdir')" || exit 1
 
@@ -45,7 +45,7 @@ index="$tmpdir/index"
 
 browse () {
 ## count slashes in the url, if we have 4 slashes, we're missing the trailing slash, add it.
-sc="$(echo "$url"| grep -o '/'| wc -l)" && [ "$sc" = "4" ] && sc="$((sc + 1))" && url="${url}/"
+sc="$(printf "%s" "$url"| grep -o '/'| wc -l)" && [ "$sc" = "4" ] && sc="$((sc + 1))" && url="${url}/"
 
 ## get $location to figure out what we're dealing with
 location="$(curl -sLI "$url"| grep -o 'location.*'| grep -v '.onion')"
@@ -57,15 +57,15 @@ location="$(curl -sLI "$url"| grep -o 'location.*'| grep -v '.onion')"
 
 [ -n "$location" ] && {
 
-last_char="$(echo "$location"| grep -o 'items.*'| tail -n1| rev| cut -c 2)"
+last_char="$(printf "%s" "$location"| grep -o 'items.*'| tail -n1| rev| cut -c 2)"
 
 [ "$last_char" = '/' ] && sc="$((sc + 1))" && url="${url}/"
 
 ## should $last_char NOT be equal to '/' then our input must be a direct link to a file
 ## in which case, we get rid of the filename in $url and assign it to $chosen_item
 [ "$last_char" != '/' ] && {
-chosen_item="$(echo "$url"| rev| cut -d '/' -f 1| rev)"
-url="$(echo "$url"| rev| cut -d '/' -f 2-| rev)"
+chosen_item="$(printf "%s" "$url"| rev| cut -d '/' -f 1| rev)"
+url="$(printf "%s" "$url"| rev| cut -d '/' -f 2-| rev)"
 sc="$((sc - 1))"; url="${url}/" ;}
 }
 
@@ -73,12 +73,12 @@ sc="$((sc - 1))"; url="${url}/" ;}
 curl -sLo "$raw_html" "$url"
 
 ## check for 404 and exit if we got one
-grep -q '404 Not Found' "$raw_html" && { echo; echo "--> ERROR: 404"; echo; exit 1 ;}
+grep -q '404 Not Found' "$raw_html" && printf "\n--> ERROR 404\n\n" && exit 1
 
 ## truncate $raw_html to allow for faster processing
 grep -A9999 '<tbody>' "$raw_html"| grep -B9999 '</tbody>' > "$trun_html"
 
-## $items contains the array of filenames. 
+## $items contains the array of filenames.
 ## delim = '^', f1=urlencoded-filenames f2=human-readable-filenames (we obviously need both..)
 ## EXAMPLE OUTPUT: 'The-Pigeons-Around-Here-Aren%27t-Real.mp3^The-Pigeons-Around-Here-Aren't-Real.mp3'
 grep -o '<a href=".*</td>' "$trun_html"| tr '>' '^'| tr '<' '"'| cut -d '"' -f 3-4| sed -e 's#"^#^#g' -e 's#\&amp;#\&#g' > "$items"
@@ -106,14 +106,14 @@ chosen_item_hr="$(grep -m1 "$chosen_item" "$index"| cut -d '^' -f 2)"
 }
 
 ## exit if user didn't choose an item, or there was no match when double checking
-[ -z "$chosen_item" ] && { echo; echo "--> NO ITEM CHOSEN."; echo; exit 1 ;}
+[ -z "$chosen_item" ] && printf "\n--> NO ITEM CHOSEN.\n\n" && exit 1
 
 ## check if the item had a size of '-' <-- NOTE: this indicates a directory
 chosen_item_fsize="$(grep -m1 "$chosen_item" "$index"| cut -d '^' -f 3)"
 
 ## if the $chosen_item is <UP>, decrease $sc by one, edit $url & run browse again
 [ "$chosen_item" = '<UP>' ] && sc="$((sc - 1))" && \
-url="$(echo "$url"| cut -d '/' -f 1-"${sc}")" && chosen_item="" && browse
+url="$(printf "%s" "$url"| cut -d '/' -f 1-"${sc}")" && chosen_item="" && browse
 
 ## if the $chosen_item is a file, add it to $url and start the downloader
 [ "$chosen_item_fsize" != '<DIR>' ] && url="${url}${chosen_item}" && download
@@ -126,10 +126,10 @@ chosen_item=""; chosen_item_fsize=""; browse ;}
 
 
 download () {
-## if the '-fn' option was specified, only echo the filename
-[ -n "$fn" ] && echo "$chosen_item_hr"
+## if the '-fn' option was specified, only print the filename
+[ -n "$fn" ] && printf "%s" "$chosen_item_hr"
 ## otherwise show everything
-[ -z "$fn" ] && { echo; echo "--> DOWNLOADING: $chosen_item_hr ~ $chosen_item_fsize"; echo ;}
+[ -z "$fn" ] && printf "%s" "\n--> DOWNLOADING: $chosen_item_hr ~ $chosen_item_fsize\n\n"
 ## download the file and exit
 curl -#Lo "$chosen_item_hr" "$url" && exit 0 || exit 1
 }
